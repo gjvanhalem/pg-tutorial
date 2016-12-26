1c2ea0116e4e6b3c9474524333fe700686f71c37
Gert Jan van Halem <gjvhalem@tmns.com>
2016-12-26 18:25:09 +0100

    added alerting

--- js/main.js
+++ js/main.js
@@ -14,8 +14,19 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new WebSqlStore();
+        var self=this;
+        this.store = new WebSqlStore(function() {
+            self.showAlert('Store initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     }
 
 };
